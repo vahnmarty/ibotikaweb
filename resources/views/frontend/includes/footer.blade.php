5f3b0bd2bfc79a2498eaeb9e4a76eeb7dd90be1a
vahnmarty <vahnmarty@gmail.com>
2018-11-28 13:33:54 +0800

    fb messenger

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -51,3 +51,20 @@
                     </div>
                 </div>
             </footer>
+
+            <!-- Load Facebook SDK for JavaScript -->
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+            <!-- Your customer chat code -->
+            <div class="fb-customerchat"
+              attribution=setup_tool
+              page_id="331524070975779"
+              theme_color="#e31d3b">
+            </div>
