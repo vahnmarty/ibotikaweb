d8c1149639a44808172d697cff5523f64df397c0
vahnmarty <vahnmarty@gmail.com>
2018-11-28 13:48:46 +0800

    fix api

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -42,10 +42,27 @@
             @include('frontend.includes.platforms')
 
             @include('frontend.includes.footer')
-            
+
         </div>
         
     @include('frontend.includes.scripts')
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your customer chat code -->
+    <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="331524070975779"
+      theme_color="#e31d3b">
+    </div>
+    
     </body>
 </html>
                 
